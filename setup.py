7d9c11481e96ad64d92b98b3e79191371fafc0f8
Tom Frederik <tlieberum@outlook.de>
2022-03-10 09:39:30 +0100

    fix python version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='pysvelte',
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
-    python_requires='>=3.9.0',
+    python_requires='>=3.6.0',
     install_requires=[
-        'torch>=1.10.1',
-        'einops>=0.3.2',
-        'numpy>=1.21.2',
+        'torch',
+        'einops',
+        'numpy',
         "typeguard",    
     ]
 )
